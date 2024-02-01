cc504f2950625780af792937982cf2971c3391cb
modiku <1042065630@qq.com>
2024-02-01 15:19:08 +0800

    更新price

--- index.ts
+++ index.ts
@@ -19,19 +19,33 @@ interface myUser{
     myAuthority?:string
     userAvaterUrl?:string
     description?:string
-
+    orders?:Order[]
 }
 
 interface Product{
     id: string
     name: string
     phone: string
+    price:number
     date:Date
     imgUrl: string
     CarouselImg1: string
     CarouselImg2: string
     CarouselImg3: string
     description: string
+}
+
+interface myProduct{
+    id: string
+    name: string
+    phone: string
+    price:number
+    date?:Date
+    imgUrl?: string
+    CarouselImg1?: string
+    CarouselImg2?: string
+    CarouselImg3?: string
+    description?: string
 }
 
 interface Order{
